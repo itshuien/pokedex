ad3fff50949421122f9104364d9fa0613d4e059d
itshuien <huien_wong@hotmail.com>
2021-02-14 00:09:49 +0800

    Create HomeStack navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { registerRootComponent } from 'expo';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import HomeStack from './routes/HomeStack';
 
 function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar barStyle="dark-content" />
+        <HomeStack />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
---
+++ src/routes/HomeStack.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from '../screens/Home';
+import PokemonDetails from '../screens/PokemonDetails';
+
+const Stack = createStackNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerShown: false,
+      headerTransparent: true,
+    }}
+  >
+    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="PokemonDetails" component={PokemonDetails} />
+  </Stack.Navigator>
+)
+
+export default HomeStack;
