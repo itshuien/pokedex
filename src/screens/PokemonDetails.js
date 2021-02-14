23b05707b7b49740c5f2193203562d3f3b404b24
itshuien <huien_wong@hotmail.com>
2021-02-14 11:50:41 +0800

    Add dynamic content to PokemonDetails

--- src/screens/PokemonDetails.js
+++ src/screens/PokemonDetails.js
@@ -1,34 +1,95 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { AntDesign } from '@expo/vector-icons';
+import axios from 'axios';
+import { pokemonTypeColors } from '../utils/colors';
 
-export default function PokemonDetails() {
+import PokemonProfileImage from '../components/PokemonProfileImage';
+import PokemonTypeLabel from '../components/PokemonTypeLabel';
+
+export default function PokemonDetails({ route, navigation }) {
+  const { name } = route.params;
+  const [pokemon, setPokemon] = useState(null);
+  const [pokemonId, setPokemonId] = useState(null);
+  const [themeColor, setThemeColor] = useState('');
+
+  const getPokemon = async () => {
+    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+    setPokemon(data);
+  }
+  
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
+  useEffect(() => {
+    const pokemonTypeColor = pokemon ? pokemonTypeColors[pokemon.types[0].type.name] : pokemonTypeColors.unknown;
+    setThemeColor(pokemonTypeColor.normal);
+
+    if (pokemon) {
+      const fullId = pokemon.id.toString().padStart(3, '0');
+      setPokemonId(fullId);
+    }
+  }, [pokemon]);
+
+  if (pokemon) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <AntDesign
+          name="arrowleft"
+          size={24}
+          color="black"
+          onPress={() => navigation.navigate('Home')}
+          style={{ marginBottom: 16 }}
+        />
+
+        <Text style={styles.pokemonId}>#{pokemonId}</Text>
+        <Text style={styles.pokemonName}>{pokemon.name}</Text>
+
+        <View style={styles.types}>
+          {pokemon.types.map(item => (
+            <PokemonTypeLabel
+              key={item.slot}
+              typeName={item.type.name}
+              color={pokemonTypeColors[item.type.name].normal}
+            />
+          ))}
+        </View>
+
+        {pokemonId ? <PokemonProfileImage pokemonId={pokemonId} themeColor={themeColor} /> : null}
+      </SafeAreaView>
+    )
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <AntDesign
-        name="arrowleft"
-        size={24}
-        color="black"
-        style={{ marginBottom: 16 }}
-      />
-
-      <View>
-        <Text style={{ ...styles.title, color: '#333' }}>Pokemon Details</Text>
-      </View>
+      <Text>Loading...</Text>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  header: {
+    width: Dimensions.get('screen').width
+  },
   container: {
     padding: 16,
     flex: 1,
   },
-  title: {
+  pokemonId: {
+    color: '#666',
+    fontSize: 12,
+    marginBottom: 4,
+  },
+  pokemonName: {
+    color: '#333',
     fontSize: 32,
     fontWeight: 'bold',
     textTransform: 'capitalize',
     marginBottom: 8,
+  },
+  types: {
+    flexDirection: 'row',
+    marginBottom: 16,
   },
 })
