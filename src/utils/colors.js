9dc631de5086d3d6e2f9d928d81fba75cb7279ff
itshuien <huien_wong@hotmail.com>
2021-02-14 11:26:40 +0800

    Create util for pokemon type colors

---
+++ src/utils/colors.js
@@ -0,0 +1,62 @@
+export const pokemonTypeColors = {
+  bug: {
+    normal: '#ef8130',
+  },
+  dark: {
+    normal: '#705848',
+  },
+  dragon: {
+    normal: '#6f39f8',
+  },
+  electric: {
+    normal: '#f9d130',
+  },
+  fairy: {
+    normal: '#f0b5bc',
+  },
+  fighting: {
+    normal: '#c03129',
+  },
+  fire: {
+    normal: '#ef8030',
+  },
+  flying: {
+    normal: '#a990f0',
+  },
+  ghost: {
+    normal: '#705898',
+  },
+  grass: {
+    normal: '#79c84f',
+  },
+  ground: {
+    normal: '#e0c068',
+  },
+  ice: {
+    normal: '#99d7d8',
+  },
+  normal: {
+    normal: '#a8a877',
+  },
+  poison: {
+    normal: '#a0419e',
+  },
+  psychic: {
+    normal: '#f85888',
+  },
+  rock: {
+    normal: '#b8a138',
+  },
+  shadow: {
+    normal: '#725b95',
+  },
+  steel: {
+    normal: '#b8b8d0',
+  },
+  unknown: {
+    normal: '#888888',
+  },
+  water: {
+    normal: '#6790f0',
+  },
+}
