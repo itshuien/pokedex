47a08e13e187c2c6947cd39f913d240d767aa411
itshuien <huien_wong@hotmail.com>
2021-02-15 11:32:11 +0800

    Change paginator styles

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, FlatList, Button, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import axios from 'axios';
 
@@ -9,6 +9,7 @@ export default function Home({ navigation }) {
   const [pokemons, setPokemons] = useState(null);
   const [previousUrl, setPreviousUrl] = useState(null);
   const [nextUrl, setNextUrl] = useState(null);
+  const pokemonFlatListRef = useRef();
 
   const resetUrls = () => {
     setPreviousUrl(null);
@@ -16,8 +17,18 @@ export default function Home({ navigation }) {
     getPokemons();
   }
 
+  const goToPage = url => {
+    getPokemons(url);
+    pokemonFlatListRef.current.scrollToOffset({ animated: true, offset: 0 });
+  }
+
+  const getPaginatorButtonColor = buttonType => {
+    const isActive = buttonType === 'previous' ? previousUrl : nextUrl;
+    return isActive ? '#237eed' : '#aaa';
+  }
+
   const getPokemons = async (url) => {
-    const result = await axios.get(url || 'https://pokeapi.co/api/v2/pokemon/');
+    const result = await axios.get(url || 'https://pokeapi.co/api/v2/pokemon/?limit=50');
     setPokemons(result.data.results);
     setPreviousUrl(result.data.previous);
     setNextUrl(result.data.next);
@@ -33,13 +44,19 @@ export default function Home({ navigation }) {
         <Text style={styles.h1}>Pokemons</Text>
       </TouchableOpacity>
 
-      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-        <Button title="Previous" onPress={() => getPokemons(previousUrl)} disabled={previousUrl === null} />
-        <Button title="Next" onPress={() => getPokemons(nextUrl)} disabled={nextUrl === null} />
+      <View style={styles.paginator}>
+        <TouchableOpacity style={styles.paginatorButton} onPress={() => goToPage(previousUrl)}>
+          <Text style={{ ...styles.paginatorButtonText, color: getPaginatorButtonColor('previous') }}>Previous</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.paginatorButton} onPress={() => goToPage(nextUrl)}>
+          <Text style={{ ...styles.paginatorButtonText, color: getPaginatorButtonColor('next') }}>Next</Text>
+        </TouchableOpacity>
       </View>
 
       <FlatList
-        style={{ flexGrow: 1, flex: 1 }}
+        ref={pokemonFlatListRef}
+        style={styles.flatList}
         data={pokemons}
         keyExtractor={(item) => item.name}
         renderItem={({item}) => (
@@ -54,16 +71,34 @@ export default function Home({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 16,
     flex: 1,
   },
   h1: {
     fontSize: 28,
     fontWeight: 'bold',
-    marginVertical: 24,
+    marginVertical: 8,
+    paddingHorizontal: 16,
   },
   h3: {
     fontSize: 16,
     marginBottom: 20,
   },
+  paginator: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    marginBottom: 8,
+  },
+  paginatorButton: {
+    paddingVertical: 8,
+  },
+  paginatorButtonText: {
+    fontSize: 16,
+  },
+  flatList: {
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 16,
+  }
 })
