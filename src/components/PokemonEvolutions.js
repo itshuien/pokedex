a1dd77eb645fe58580282bf6d938d2527444160a
itshuien <huien_wong@hotmail.com>
2021-02-18 10:16:10 +0800

    Create useEvolutions custom hook

--- src/components/PokemonEvolutions.js
+++ src/components/PokemonEvolutions.js
@@ -1,42 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import axios from 'axios';
-
-import { getEvolutionChain } from '../utils/pokedexApi';
+import useEvolutions from '../hooks/useEvolutions';
 
 export default function PokemonEvolutions({ pokemonName }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [evolutions, setEvolutions] = useState([]);
-
-  const fetchEvolutionChain = async (source) => {
-    try {
-      const evolutionChain = await getEvolutionChain(source, pokemonName);
-      setEvolutions(evolutionChain);
-    } catch(e) {
-      if (!isLoading) return;
-      console.error(e);
-    }
-  }
-
-  useEffect(() => {
-    setIsLoading(true);
-    const source = axios.CancelToken.source();
-    fetchEvolutionChain(source);
-
-    return () => {
-      setIsLoading(false);
-      source.cancel('Cancel axios request');
-    }
-  }, []);
+  const { evolutions } = useEvolutions(pokemonName);
 
   return (
-    <View style={styles.container}>
-      {evolutions.map((evolution, index) => {
+    <View>
+      {evolutions.slice(1).map((evolution, index) => {
+        const evolvesFrom = evolutions.find(item => item.name === evolution.evolvesFrom);
+
         return (
           <View key={index} style={styles.evolution}>
-            <Text style={{ color: '#888' }}>#{evolution.fullId}</Text>
-            <Text style={{ textAlign: 'center', textTransform: 'capitalize' }}>{evolution.name}</Text>
-            <Image style={styles.image} resizeMethod="scale" source={{ uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${evolution.fullId}.png` }} />
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ color: '#888' }}>#{evolvesFrom.fullId}</Text>
+              <Text style={{ textAlign: 'center', textTransform: 'capitalize' }}>{evolvesFrom.name}</Text>
+              <Image style={styles.image} resizeMethod="scale" source={{ uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${evolvesFrom.fullId}.png` }} />
+            </View>
+            <View style={styles.trigger}>
+              {evolution.trigger === 'level-up' ? <Text>Lv {evolution.minLevel}</Text> : null}
+              <Text>{evolution.trigger}</Text>
+            </View>
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ color: '#888' }}>#{evolution.fullId}</Text>
+              <Text style={{ textAlign: 'center', textTransform: 'capitalize' }}>{evolution.name}</Text>
+              <Image style={styles.image} resizeMethod="scale" source={{ uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${evolution.fullId}.png` }} />
+            </View>
           </View>
         )
       })}
@@ -45,15 +34,18 @@ export default function PokemonEvolutions({ pokemonName }) {
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
   evolution: {
+    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 24,
+    marginBottom: 40,
+  },
+  trigger: {
+    alignItems: 'center',
+    marginHorizontal: 24,
   },
   image: {
-    width: 100,
-    height: 100,
+    width: 88,
+    height: 88,
   }
 })
---
+++ src/hooks/useEvolutions.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function useEvolutions(pokemonName) {
+  const [evolutions, setEvolutions] = useState([]);
+  const [error, setError] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const cancelTokenSource = axios.CancelToken.source();
+
+  const fetchEvolutions = async () => {
+    try {
+      const { data: pokemonSpecies } = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}/`, { cancelToken: cancelTokenSource.token });
+      const { data: { chain } } = await axios.get(pokemonSpecies.evolution_chain.url, { cancelToken: cancelTokenSource.token });
+
+      const evolutionChain = flattenEvolutionChain([chain]);
+
+      const result = await Promise.all(evolutionChain.map(async evolution => {
+        const details = await getPokemonDetails(evolution.name);
+        return { ...evolution, ...details }
+      }))
+
+      setEvolutions(result);
+    } catch(error) {
+      console.log(error)
+      if (!isFetching) return;
+      setError(error);
+    }
+  }
+  
+  useEffect(() => {
+    setIsFetching(true);
+    fetchEvolutions();
+
+    return () => {
+      setIsFetching(false);
+      cancelTokenSource.cancel();
+    }
+  }, [])
+
+  return { evolutions, error };
+}
+
+function flattenEvolutionChain(chain) {
+  const traverseResult = [];
+  traverseEvolutionChain(null, chain, 1);
+  return traverseResult;
+
+  function traverseEvolutionChain(evolvesFrom, evolutionChain, evolutionStage) {
+    if (!evolutionChain.length) return evolutionChain;
+  
+    for (let item of evolutionChain) {
+      const { species, evolution_details } = item;
+
+      const data = {
+        evolutionStage,
+        evolvesFrom,
+        name: species.name,
+        trigger: evolution_details.length ? evolution_details[0].trigger.name : null,
+        minLevel: evolution_details.length ? evolution_details[0].min_level : null,
+        item: evolution_details.length ? evolution_details[0].item : null,
+      }
+      traverseResult.push(data);
+  
+      traverseEvolutionChain(item.species.name, item.evolves_to, evolutionStage + 1);
+    }
+  }
+}
+
+async function getPokemonDetails(pokemonName) {
+  const { data: { id } } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
+
+  return {
+    id,
+    fullId: id.toString().padStart(3, '0')
+  }
+}
